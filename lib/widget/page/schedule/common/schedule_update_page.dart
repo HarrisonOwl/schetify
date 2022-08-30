a770af0fead93c896d9112a42e271aba643a6e6f
Thelema4020 <tomiiekoichiro@fuukakouichirou-no-iMac.local>
2022-08-30 17:13:06 +0900

    ListView

--- lib/widget/page/schedule/common/schedule_update_page.dart
+++ lib/widget/page/schedule/common/schedule_update_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ScheduleUpdatePage extends HookConsumerWidget {
-  const ScheduleUpdatePage({Key? key}) : super(key: key);
+  ScheduleUpdatePage({Key? key}) : super(key: key);
+  final List<String> buttonName = <String>['A', 'B', 'C'];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -10,16 +11,58 @@ class ScheduleUpdatePage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text("予定作成&編集"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Settings',
-            )
-          ],
-        ),
-      ),
+      body: Align(
+          alignment: Alignment.bottomCenter,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: buttonName.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed("/");
+                    },
+                    child: Text('Entry ${buttonName[index]}')
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          )
+      )
+      // body: ListView.separated(
+      //   padding: const EdgeInsets.all(8),
+      //   itemCount: buttonName.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return Container(
+      //         height: 50,
+      //         // padding: const EdgeInsets.all(8.0), //マージン
+      //         child: ElevatedButton(
+      //             onPressed: () {
+      //               Navigator.of(context).pushNamed("/");
+      //             },
+      //             child: Text('Entry ${buttonName[index]}')
+      //         ),
+      //     );
+      //   },
+      //   separatorBuilder: (BuildContext context, int index) => const Divider(),
+      // )
     );
   }
 }
+
+
+
+// Padding(
+//   padding: const EdgeInsets.all(8.0), //マージン
+//   child: SizedBox(
+//       height: 50,
+//       width: 200,
+//       child: ElevatedButton(
+//           onPressed: () {
+//             Navigator.of(context).pushNamed("/");
+//           },
+//           child: Text('Log In')
+//       )
+//   ),
+// ),
