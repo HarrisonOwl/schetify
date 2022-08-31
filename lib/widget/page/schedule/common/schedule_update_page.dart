1126b501639af8b342cf98015436421c7c29e9d4
Thelema4020 <tomiiekoichiro@fuukakouichirou-no-iMac.local>
2022-09-01 02:22:49 +0900

    fin

--- lib/widget/page/schedule/common/schedule_update_page.dart
+++ lib/widget/page/schedule/common/schedule_update_page.dart
@@ -8,16 +8,17 @@ List<Schedule> scheduleList = [];
 
 class ScheduleUpdatePage extends HookConsumerWidget {
   ScheduleUpdatePage({Key? key}) : super(key: key);
-  final List<String> buttonName = <String>[
+  List<String> tileName = <String>[
     "日にち設定",
     "席分け設定",
     "ラベル設定",
     "割り勘設定",
-    "目的地設定",
+    "目的地:",
     "アンケート設定",
-    "出席確認on/off",
-    "ユーザー設定画面(url発行)",
+    "出席",
+    "シェア(URL)",
   ];
+
   final List<String> routeName = <String>[
     "/schedule/new/day",
     "/schedule/new/seat",
@@ -25,7 +26,24 @@ class ScheduleUpdatePage extends HookConsumerWidget {
     "/schedule/new/cost",
     "/schedule/new/destination",
     "/schedule/new/questionnaire",
+    "/schedule/new/questionnaire",
+    "/schedule/new/questionnaire",
   ];
+
+  final List<Icon?> iconName = <Icon?>[
+    const Icon(Icons.calendar_today),
+    const Icon(Icons.table_bar),
+    const Icon(Icons.label),
+    const Icon(Icons.money),
+    const Icon(Icons.room),
+    const Icon(Icons.feed),
+    null,
+    null,
+  ];
+
+  // tentative variable
+  String toggleState = "ON";
+  String address = "東京都世田谷区北沢３丁目２３−１４";
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -33,42 +51,105 @@ class ScheduleUpdatePage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text("予定作成&編集"),
       ),
-      body: Align(
-          alignment: Alignment.bottomCenter,
-          child: ListView.separated(
-            padding: const EdgeInsets.all(8),
-            itemCount: buttonName.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 50,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(routeName[index]);
-                    },
-                    child: Text(buttonName[index])
+      body: Column(
+        children: [
+          Container(
+            alignment: const Alignment(0.0, 0.0),
+            height: 100,
+            child: TextFormField(
+              style: const TextStyle(
+                fontSize: 30,
+              ),
+              decoration: InputDecoration(
+                hintText: '予定名',
+                contentPadding: const EdgeInsets.all(20),
+                fillColor: Colors.green[100],
+                filled: true,
+                isDense: true,
+                prefixIcon: Container(
+                  margin: const EdgeInsets.all(20),
+                  child: const Icon(
+                    Icons.event_available,
+                    size: 40,
+                  ),
                 ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: ListView.separated(
+            itemCount: tileName.length,
+            itemBuilder: (BuildContext context, int index) {
+              return SubListItem(
+                title: tileName[index],
+                leading: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minHeight: 44,
+                    minWidth: 34,
+                    maxHeight: 64,
+                    maxWidth: 54),
+                  child: iconName[index],
+                ),
+                route: routeName[index],
+                toggle: tileName[index] == '出席' ? toggleState : '',
+                address: tileName[index] == '目的地:' ? address : '',
               );
             },
             separatorBuilder: (BuildContext context, int index) => const Divider(),
+            ),
           )
+        ],
       )
-      // body: ListView.separated(
-      //   padding: const EdgeInsets.all(8),
-      //   itemCount: buttonName.length,
-      //   itemBuilder: (BuildContext context, int index) {
-      //     return Container(
-      //         height: 50,
-      //         // padding: const EdgeInsets.all(8.0), //マージン
-      //         child: ElevatedButton(
-      //             onPressed: () {
-      //               Navigator.of(context).pushNamed("/");
-      //             },
-      //             child: Text('Entry ${buttonName[index]}')
-      //         ),
-      //     );
-      //   },
-      //   separatorBuilder: (BuildContext context, int index) => const Divider(),
-      // )
+    );
+  }
+}
+
+
+class SubListItem extends StatelessWidget {
+  final String title;
+  final Widget leading;
+  final String route;
+  final String toggle;
+  final String address;
+
+  SubListItem({required this.title, required this.leading, required this.route, required this.toggle, required this.address});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(title),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(address),
+        ],
+      ),
+      leading: leading,
+      onTap: () => {
+        Navigator.of(context).pushNamed(route)
+      },
+      // onLongPress: () => {},
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            toggle,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            width: 50,
+          ),
+          const Icon(Icons.arrow_forward)
+        ],
+      ),
     );
   }
 }
