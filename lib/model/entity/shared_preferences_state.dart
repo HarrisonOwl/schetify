5917bde5ed821e7438eaadc02069ee446e7cc3dd
SEED0228 <naokitane6828@gmail.com>
2022-09-04 00:43:17 +0900

    freezedで生成されたファイルをフォルダに保管するよう修正

--- lib/model/entity/shared_preferences_state.dart
+++ lib/model/entity/shared_preferences_state.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // {ファイル名}.freezed.dart　と書く
-part 'shared_preferences_state.freezed.dart';
+part 'generated/shared_preferences_state.freezed.dart';
 
 //Freezed特有の書き方なので、スニペットを用意するのが良い
 @freezed
