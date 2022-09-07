f7d4b0e8d2af054c73a3fc771521a949d9e4fcbd
SEED0228 <naokitane6828@gmail.com>
2022-09-07 14:56:29 +0900

    グルーピングの処理を修正

--- lib/provider/grouping_provider.dart
+++ lib/provider/grouping_provider.dart
@@ -1,27 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../model/entity/grouped_user_list.dart';
-import '../model/entity/number_of_groups.dart';
 import '../model/entity/user_name.dart';
 import '../model/entity/user_name_list.dart';
 
 class GroupedUserListNotifier extends StateNotifier<GroupedUserList> {
   GroupedUserListNotifier() : super(const GroupedUserList(groupedUserList: []));
 
-  final a = const UserNameList(userNameList: [UserName(name: "a"),UserName(name: "b"),UserName(name: "c"),UserName(name: "d"),UserName(name: "e"),UserName(name: "f")]);
+  final a = const UserNameList(userNameList: [UserName(name: "a"),UserName(name: "b"),UserName(name: "c"),UserName(name: "d"),UserName(name: "e"),UserName(name: "f"),UserName(name: "g"),UserName(name: "h"),UserName(name: "i"),UserName(name: "j"),UserName(name: "k")]);
 
-  void grouping(int numberOfGroups) {
-    List<UserName> shuffled_list = [...a.userNameList];
-    shuffled_list.shuffle();
-
+  void grouping(int num) {
+    List<UserName> shuffledList = [...a.userNameList];
+    shuffledList.shuffle();
+    final length = shuffledList.length;
+    List<int> groupNumList = [];
+    for(var i = 0; i < length ~/ num; i++) {
+      groupNumList.add(num);
+    }
+    var rem = length % num;
+    if(rem != 0) {
+      var idx = 0;
+      while(true) {
+        if(groupNumList.reduce(max) - rem < 2) {
+          groupNumList.add(rem);
+          break;
+        }
+        groupNumList[idx % length]--;
+        rem++;
+        idx++;
+      }
+    }
     List<UserNameList> groupedUserList = [];
-    final chunkSize = numberOfGroups;
 
-    for (var i = 0; i < shuffled_list.length; i += chunkSize) {
-      final isLast = i + chunkSize >= shuffled_list.length;
-      final end = isLast ? shuffled_list.length : i + chunkSize;
-      final element = UserNameList(userNameList: shuffled_list.sublist(i, end));
+    var sum = 0;
+    for (var i = 0; i < groupNumList.length; i++) {
+      final end = sum + groupNumList[i];
+      final element = UserNameList(userNameList: shuffledList.sublist(sum, end));
+      sum = end;
       groupedUserList.add(element);
     }
 
