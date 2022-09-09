b689ae0c74251a6b9095916326893e3d61812fbd
SEED0228 <naokitane6828@gmail.com>
2022-09-09 14:52:46 +0900

    パスワード変更画面のUI改善

--- lib/widget/components/setting/change_password_page.dart
+++ lib/widget/components/setting/change_password_page.dart
@@ -13,72 +13,140 @@ class ChangePasswordPage extends HookConsumerWidget {
 
     return Scaffold(
       resizeToAvoidBottomInset : false,
+      backgroundColor: Colors.green,
       appBar: AppBar(
         bottomOpacity: 0.0,
         elevation: 0.0,
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.green,
-            child: Padding(padding: const EdgeInsets.all(20),
-              child: SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.password,
-                      size: 40,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 10),
-                    Text( 'パスワード変更' , style: TextStyle(color: Colors.white, fontSize: 26.0,), textAlign: TextAlign.left),
-                  ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.green,
+              child: Padding(padding: const EdgeInsets.all(20),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.password,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 10),
+                      Text( 'パスワード変更' , style: TextStyle(color: Colors.white, fontSize: 26.0,), textAlign: TextAlign.left),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'パスワード',
-                    prefixText: 'パスワード',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+                      Container(
+                        alignment: const Alignment(0.0, 0.0),
+                        height: 100,
+                        child: TextFormField(
+                          obscureText: true,
+                          initialValue: password.value,
+                          onChanged: (text){
+                            password.value = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'パスワード',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            contentPadding: const EdgeInsets.all(20),
+                            isDense: true,
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  obscureText: true,
-                  textAlign: TextAlign.end,
-                  initialValue: password.value,
-                  onChanged: (text){
-                    password.value = text;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: '再入力',
-                    prefixText: '再入力',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  Column(
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                      Container(
+                        alignment: const Alignment(0.0, 0.0),
+                        height: 100,
+                        child: TextFormField(
+                          obscureText: true,
+                          initialValue: passwordAgain.value,
+                          onChanged: (text){
+                            passwordAgain.value = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: '再入力',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            contentPadding: const EdgeInsets.all(20),
+                            isDense: true,
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  obscureText: true,
-                  textAlign: TextAlign.end,
-                  initialValue: passwordAgain.value,
-                  onChanged: (text){
-                    passwordAgain.value = text;
-                  },
-                ),
-                ListTile(
-                  title: const Text('パスワード変更'),
-                  onTap: () {
-                    int count = 0;
-                    Navigator.popUntil(context, (_) => count++ >= 2);
-                  },
-                ),
-              ],
-            ),
-          )
-        ],
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 70,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35),
+                            )
+                        ),
+                        onPressed: () {
+                          int count = 0;
+                          Navigator.popUntil(context, (_) => count++ >= 2);
+                        },
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.grey,
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widget/components/setting/enter_password_page.dart
+++ lib/widget/components/setting/enter_password_page.dart
@@ -13,54 +13,95 @@ class EnterPasswordPage extends HookConsumerWidget {
 
     return Scaffold(
       resizeToAvoidBottomInset : false,
+      backgroundColor: Colors.green,
       appBar: AppBar(
         bottomOpacity: 0.0,
         elevation: 0.0,
       ),
       body: Column(
         children: [
-          Container(
-            color: Colors.green,
-            child: Padding(padding: const EdgeInsets.all(20),
-              child: SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.password,
-                      size: 40,
-                      color: Colors.white,
+          Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.green,
+                    child: Padding(padding: const EdgeInsets.all(20),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.password,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text( 'パスワード変更' , style: TextStyle(color: Colors.white, fontSize: 26.0,), textAlign: TextAlign.left),
+                          ],
+                        ),
+                      ),
                     ),
-                    SizedBox(width: 10),
-                    Text( 'パスワード変更' , style: TextStyle(color: Colors.white, fontSize: 26.0,), textAlign: TextAlign.left),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'パスワード確認',
-                    prefixText: 'パスワード確認',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
                   ),
-                  obscureText: true,
-                  textAlign: TextAlign.end,
-                  initialValue: password.value,
-                  onChanged: (text){
-                    password.value = text;
-                  },
-                ),
-                ListTile(
-                    title: const Text('次へ'),
-                    onTap: () => Navigator.pushNamed(context, '/settings/account/password/change')
-                ),
-              ],
-            ),
+                  Column(
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.12),
+                      Container(
+                        alignment: const Alignment(0.0, 0.0),
+                        height: 100,
+                        child: TextFormField(
+                          obscureText: true,
+                          initialValue: password.value,
+                          onChanged: (text){
+                            password.value = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'パスワード確認',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            contentPadding: const EdgeInsets.all(20),
+                            isDense: true,
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100.0, left: 30.0, right: 30.0, bottom: 30.0),
+                    child: SizedBox(
+                      height: 70,
+                      width: 70,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35),
+                            )
+                        ),
+                        onPressed: () => Navigator.pushNamed(context, '/settings/account/password/change'),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.grey,
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
           )
         ],
       ),
